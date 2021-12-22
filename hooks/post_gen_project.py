ae9bcaebde9d09312dc602f79b77afef07142ce3
Tobias Raabe <raabe@posteo.de>
2021-12-22 15:44:42 +0100

    Commit new hooks.

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,64 @@
+"""This module contains hooks which are executed after the template is rendered."""
+import shutil
+import subprocess
+import warnings
+from pathlib import Path
+
+
+def remove_file(*filepath):
+    """Remove a file."""
+    try:
+        Path(*filepath).unlink()
+    except FileNotFoundError:
+        pass
+
+
+def remove_directory(*filepath):
+    """Remove a directory."""
+    try:
+        path = Path(*filepath)
+        shutil.rmtree(path)
+    except FileNotFoundError:
+        pass
+
+
+def main():
+    """Apply post generation hooks."""
+    project_path = Path.cwd()
+
+    if "{{ cookiecutter.create_changelog }}" == "no":
+        remove_file(project_path, "CHANGES.rst")
+
+    if "{{ cookiecutter.open_source_license }}" == "Not open source":
+        remove_file(project_path, "LICENSE")
+
+    if "{{ cookiecutter.add_tox }}" == "no":
+        remove_directory(project_path, ".github", "workflows")
+        remove_file("tox.ini")
+
+    if "{{ cookiecutter.add_github_actions }}" == "no":
+        remove_directory(project_path, ".github", "workflows")
+
+    if "{{ cookiecutter.add_readthedocs }}" == "no":
+        remove_file(project_path, ".readthedocs.yaml")
+
+    subprocess.run(("git", "init"), check=True)
+    subprocess.run(("git", "branch", "-m", "main"), check=True)
+
+    if "{{ cookiecutter.create_conda_environment_at_finish }}" == "yes":
+        if shutil.which("mamba") is not None:
+            conda_exe = shutil.which("mamba")
+        else:
+            conda_exe = shutil.which("conda")
+
+        if conda_exe is None:
+            warnings.warn(
+                "conda environment could not be created since no conda or mamba "
+                "executable was found."
+            )
+        else:
+            subprocess.run(("conda", "env", "create"), check=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,40 @@
+"""This module contains hooks which are executed before the template is rendered."""
+import re
+
+MODULE_REGEX = r"^[-_a-zA-Z0-9]*$"
+ENVIRON_REGEX = r"^[-_a-zA-Z0-9]*$"
+PYTHONVERSION_REGEX = r"^(3)\.[6-9]$"
+
+EXCEPTION_MSG_MODULE_NAME = """
+ERROR: The project slug ({module_name}) is not a valid Python module name.
+Please do not use anything other than letters, numbers, underscores '_',
+and minus signs '-'.
+"""
+
+EXCEPTION_MSG_ENVIRON_NAME = """
+ERROR: The project slug ({environment_name}) is not a valid conda environment name.
+Please do not use anything other than letters, numbers, underscores '_',
+and minus signs '-'.
+"""
+
+
+def main():
+    """Apply pre-generation hooks."""
+    module_name = "{{ cookiecutter.project_slug}}"
+
+    if not re.match(MODULE_REGEX, module_name):
+        raise ValueError(EXCEPTION_MSG_MODULE_NAME.format(module_name))
+
+    environment_name = "{{ cookiecutter.conda_environment_name }}"
+
+    if not re.match(ENVIRON_REGEX, environment_name):
+        raise ValueError(EXCEPTION_MSG_ENVIRON_NAME.format(environment_name))
+
+    python_version = "{{ cookiecutter.python_version }}"
+
+    if not re.match(PYTHONVERSION_REGEX, python_version):
+        raise ValueError("ERROR: The python version must be >= 3.6")  # noqa: TC003
+
+
+if __name__ == "__main__":
+    main()
