b42fe0187dc7d25262aedc8b73f82c8d18613f9e
Tobias Raabe <raabe@posteo.de>
2022-02-09 02:11:40 +0100

    Deprecate Python 3.6, add support for Python 3.10 and add mypy optionally. (#9)

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/config.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/config.py
@@ -5,8 +5,6 @@ from pathlib import Path
 try:
     from ._version import version as __version__
 except ImportError:
-    # broken installation, we don't even try unknown only works because we do poor mans
-    # version compare
     __version__ = "unknown"
 
 
