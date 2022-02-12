4dac3811f1120a4c45ba8cb481980264b0294d7c
Tobias Raabe <raabe@posteo.de>
2022-02-12 18:24:03 +0100

    More docs stuff.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,19 @@ version = "1.1"
 
 # Add any Sphinx extension module names here, as strings. They can be extensions coming
 # with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = [
+    "IPython.sphinxext.ipython_console_highlighting",
+    "IPython.sphinxext.ipython_directive",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_copybutton",
+    "sphinx_panels",
+    "autoapi.extension",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -29,14 +41,38 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and directories to
 # ignore when looking for source files. This pattern also affects html_static_path and
 # html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
+
+# Configuration for autoapi to generate and API page.
+autoapi_type = "python"
+autoapi_dirs = ["../../src"]
+autoapi_keep_files = False
+autoapi_add_toctree_entry = False
+
+# Remove prefixed $ for bash, >>> for Python prompts, and In [1]: for IPython prompts.
+copybutton_prompt_text = r"\$ |>>> |In \[\d\]: "
+copybutton_prompt_is_regexp = True
+
+# Use these roles to create links to github users and pull requests.
+extlinks = {
+    "ghuser": ("https://github.com/%s", "@"),
+    "gh": ("https://github.com/pytask-dev/cookiecutter-pytask-project/pull/%s", "#"),
+}
+
+# Link objects to other documentations.
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3.9", None),
+}
 
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages. See the documentation for a list of
+# The theme to use for HTML and HTML Help pages.  See the documentation for a list of
 # builtin themes.
 html_theme = "furo"
+
+pygments_style = "sphinx"
+pygments_dark_style = "monokai"
 
 # Add any paths that contain custom static files (such as style sheets) here, relative
 # to this directory. They are copied after the builtin static files, so a file named
