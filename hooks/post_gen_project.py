dee4355ef531ff55256998b987187dfcfd2b1ad2
Tobias Raabe <raabe@posteo.de>
2023-11-22 23:05:31 +0100

    Fix.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,4 @@
-"""This module contains hooks which are executed after the template is rendered."""
+"""Contains hooks which are executed after the template is rendered."""
 from __future__ import annotations
 
 import shutil
@@ -39,7 +39,9 @@ def main() -> None:
         remove_file(project_path, ".readthedocs.yaml")
 
     subprocess.run(
-        ("git", "init", "--initial-branch", "main"), check=True, capture_output=True
+        ("git", "init", "--initial-branch", "main"),
+        check=True,
+        capture_output=True,
     )
 
     if "{{ cookiecutter.make_initial_commit }}" == "yes":
@@ -81,7 +83,8 @@ def main() -> None:
         else:
             warnings.warn(
                 "conda environment could not be created since no conda or mamba "
-                "executable was found."
+                "executable was found.",
+                stacklevel=1,
             )
 
 
