4d445101cd1df17eab5104bc5e5d9ae6918525d3
Tobias Raabe <raabe@posteo.de>
2023-11-23 12:19:42 +0100

    Fix.

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,4 +1,4 @@
-"""This module contains the main namespace of {{ cookiecutter.project_slug }}."""
+"""Contains the main namespace of {{ cookiecutter.project_slug }}."""
 # Import the version from _version.py which is dynamically created by setuptools-scm
 # when the project is installed with ``pip install -e .``. Do not put it into version
 # control!
